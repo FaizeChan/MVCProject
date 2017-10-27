e8e22578f851e6eb26e5bc4768ff431d50e05c49
FaizeChan <obey.faize@gmail.com>
2017-10-27 12:40:53 -0400

    added Location and List under Models

---
+++ MVCProject/Models/DreamLocations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCProject.Models
+{
+    public class DreamLocation
+    {
+        [Key]
+        public int DreamLocationID { get; set; }
+        public string ItemName { get; set; }
+        public DateTime DateFounded { get; set; }
+        public string Details { get; set; }
+
+        [ForeignKey("List")]
+        public int ListID { get; set; }
+        public virtual List List { get; set; }
+    }
+}
